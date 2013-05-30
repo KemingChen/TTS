ff9b329e937f947a55a146d5881a593467cd995f
ben <ben@6b2d4326-5d3a-2644-a265-d5bffc151a22>
2013-05-30 09:47:28 +0000

    git-svn-id: https://140.124.181.190/svn/TaipeiTech@8 6b2d4326-5d3a-2644-a265-d5bffc151a22

---
+++ application/controllers/selectauthor.php
@@ -0,0 +1,30 @@
+<?php
+
+class SelectAuthor extends CI_Controller
+{
+    public function index(){
+        echo"hello";
+    }
+    
+    public function select()
+    {
+        $this->load->database();
+        echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">';
+        $query = $this->db->query("Select * From author");
+        if ($query->num_rows() > 0)
+        {
+            foreach ($query->result() as $row)
+            {
+                //echo "id: " . $row->title();
+                echo ", name: " . $row->name . "<br />";
+            }
+            echo "ff<br />";
+        }
+        else
+        {
+            echo "haha  it's nothing";  
+        }
+    }
+}
+
+?>
