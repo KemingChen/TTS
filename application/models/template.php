2e066d900f3764158acf7582dd4cae9fee78e0b1
keming <keming@6b2d4326-5d3a-2644-a265-d5bffc151a22>
2013-06-05 15:29:21 +0000

    更新 SliderBar
    git-svn-id: https://140.124.181.190/svn/TaipeiTech@26 6b2d4326-5d3a-2644-a265-d5bffc151a22

--- application/models/template.php
+++ application/models/template.php
@@ -2,45 +2,47 @@
 
 class Template extends CI_Model
 {
-    private $cSliderBar;
-    private $mSliderBar;
-    
+    private $sliderBar;
+
     public function __construct()
     {
         parent::__construct();
         $this->load->helper('url');
+        $this->initSliderBar();
     }
 
-    public function uCSliderBar($pageName, $data = array()) // using Category Slider Bar
+    // using Category Slider Bar
+    public function uCSliderBar($activeID, $pageName, $data = array())
+    {
+        $this->SliderBar("Category", $activeID, $pageName, $data);
+    }
+
+    // using Member Slider Bar
+    public function uMSliderBar($activeID, $pageName, $data = array())
+    {
+        $this->SliderBar("Member", $activeID, $pageName, $data);
+    }
+
+    private function SliderBar($SBName, $activeID, $pageName, $data)
     {
         $return = false;
         $info = array();
         $info["data"] = $data;
         $info["pageName"] = $pageName;
+        $info["menu"] = $this->getSliderBarInfo($activeID, $this->sliderBar[$SBName]);
 
         $this->load->view('include/Header', $this->getHeaderInfo());
-        $this->load->view("include/CategorySliderBar", $info, $return);
+        $this->load->view("include/SliderBar", $info, $return);
         $this->load->view('include/Footer');
     }
 
-    public function uMSliderBar($pageName, $data = array()) // using Member Slider Bar
-    {
-        $return = false;
-        $info = array();
-        $info["data"] = $data;
-        $info["pageName"] = $pageName;
-
-        $this->load->view('include/Header', $this->getHeaderInfo());
-        $this->load->view("include/MemberSliderBar", $info, $return);
-        $this->load->view('include/Footer');
-    }
-
-    private function setSliderBarInfo($activeID, &$data)
+    private function getSliderBarInfo($activeID, $data)
     {
         for ($i = 0; $i < count($data); $i++)
         {
             $data[$i]["Active"] = $data[$i]["ID"] == $activeID ? "active" : "";
         }
+        return $data;
     }
 
     private function getHeaderInfo()
@@ -49,19 +51,21 @@ class Template extends CI_Model
         $info["isLogin"] = true;
         return $info;
     }
-    
+
     private function initSliderBar()
     {
-        $cSBar = $this->cSliderBar;
+        $cSBar = array();
         array_push($cSBar, array("ID" => "C0", "Tag" => "商業理財"));
         array_push($cSBar, array("ID" => "C1", "Tag" => "文學小說"));
         array_push($cSBar, array("ID" => "C2", "Tag" => "藝術設計"));
-        
-        $mSBar = $this->mSliderBar;
-        array_push($cSBar, array("ID" => "", "Tag" => "會員資料"));
-        array_push($cSBar, array("ID" => "", "Tag" => "交易紀錄"));
-        array_push($cSBar, array("ID" => "", "Tag" => "購物車"));
-        array_push($cSBar, array("ID" => "", "Tag" => "修改密碼"));
+        $this->sliderBar["Category"] = $cSBar;
+
+        $mSBar = array();
+        array_push($mSBar, array("ID" => "Member", "Tag" => "會員資料"));
+        array_push($mSBar, array("ID" => "Record", "Tag" => "交易紀錄"));
+        array_push($mSBar, array("ID" => "ShopCar", "Tag" => "購物車"));
+        array_push($mSBar, array("ID" => "RevisePWD", "Tag" => "修改密碼"));
+        $this->sliderBar["Member"] = $mSBar;
     }
 }
 
---
+++ application/views/include/SliderBar.php
@@ -0,0 +1,29 @@
+<div class="container-fluid">
+  <div class="row-fluid">
+    <div class="span1">
+      <!--Sidebar space-->
+    </div>
+    <div class="span2">
+        <div class="sidebar-nav">
+    		<ul class="nav nav-pills nav-stacked"> 
+            <?
+                for($i=0;$i<count($menu);$i++)
+                {
+                    $active = $menu[$i]["Active"];
+                    $tag = $menu[$i]["Tag"];
+                    $id = $menu[$i]["ID"];
+                    echo "<li class='$active'>";
+                    echo "<a href='#'>";
+                    echo "<i class='icon-chevron-right'></i>$tag";
+                    echo "</a>";
+                    echo "</li>";
+                }
+            ?>
+                <li><a href="#">...施工中</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="span7">
+        <?$this->load->view($pageName, $data, false);?>
+    </div>
+  </div>
