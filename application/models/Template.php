fdd214695c16508b33d9a0ecfbb1df91f2667291
keming <keming@6b2d4326-5d3a-2644-a265-d5bffc151a22>
2013-06-06 10:20:24 +0000

    新增 空頁面 Nav 分出 Template 要用 其中的 View 去顯示
    git-svn-id: https://140.124.181.190/svn/TaipeiTech@41 6b2d4326-5d3a-2644-a265-d5bffc151a22

---
+++ application/models/Template.php
@@ -0,0 +1,92 @@
+<?php
+
+class Template extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+    }
+    
+    public function view($activeID, $sliderBarName, $pageName, $data)
+    {
+        $header = $this->getHeader($sliderBarName);
+        $sliderBar = $this->getSliderBar($activeID, $sliderBarName);
+        $this->uSliderBar($header, $sliderBar, $pageName, $data);
+    }
+
+    private function getHeader($activeID)
+    {
+        $header = array();
+        array_push($header, array("ID" => "Category", "Tag" => "瀏覽書籍"));
+        array_push($header, array("ID" => "Member", "Tag" => "會員專區"));
+        $this->setUrl($header);
+        $this->setActive($activeID, $header);
+
+        $info["isLogin"] = true;
+        $info["header"] = $header;
+        return $info;
+    }
+
+    private function getSliderBar($activeID, $sliderBarName)
+    {
+        $sliderBar = array();
+        switch ($sliderBarName)
+        {
+            case "Category":
+                array_push($sliderBar, array("ID" => "C0", "Tag" => "商業理財"));
+                array_push($sliderBar, array("ID" => "C1", "Tag" => "文學小說"));
+                array_push($sliderBar, array("ID" => "C2", "Tag" => "藝術設計"));
+                $this->setUrl($sliderBar, "Category/");
+                break;
+
+            case "Member":
+                array_push($sliderBar, array("ID" => "Member", "Tag" => "會員資料"));
+                array_push($sliderBar, array("ID" => "Record", "Tag" => "交易紀錄"));
+                array_push($sliderBar, array("ID" => "Concern", "Tag" => "關注書單"));
+                array_push($sliderBar, array("ID" => "ShopCar", "Tag" => "購物車"));
+                array_push($sliderBar, array("ID" => "RePassword", "Tag" => "修改密碼"));
+                $this->setUrl($sliderBar);
+                break;
+        }
+        $this->setActive($activeID, $sliderBar);
+        return $sliderBar;
+    }
+
+    private function setActive($activeID, &$data)
+    {
+        foreach ($data as & $item)
+        {
+            $item["Active"] = $item["ID"] == $activeID ? "active" : "";
+        }
+    }
+
+    private function setUrl(&$array, $baseUrl = "")
+    {
+        foreach ($array as & $item)
+        {
+            $item["Url"] = base_url() . "Nav/" . $baseUrl . $item["ID"];
+        }
+    }
+
+    private function uSliderBar($header, $sliderBar, $pageName, $data)
+    {
+        $return = false;
+        $info = array();
+        $info["data"] = $data;
+        $info["pageName"] = $pageName;
+        $info["menu"] = $sliderBar;
+
+        $this->load->view('include/Header', $header);
+        $this->load->view("include/SliderBar", $info, $return);
+        $this->load->view('include/Footer');
+    }
+
+    private function debug($array)
+    {
+        print_r($array);
+        exit;
+    }
+}
+
+?>
