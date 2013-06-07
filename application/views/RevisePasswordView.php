a049cdb1da1714aa282238c3ba1efca318d77165
kai <kai@6b2d4326-5d3a-2644-a265-d5bffc151a22>
2013-06-07 07:54:46 +0000

    RevisePasswordView.php complete.
    git-svn-id: https://140.124.181.190/svn/TaipeiTech@65 6b2d4326-5d3a-2644-a265-d5bffc151a22

---
+++ application/views/RevisePasswordView.php
@@ -0,0 +1,14 @@
+<div class="container-fluid">
+	<div class="row-fluid">
+		<div class="span12">
+			<form>
+				<fieldset>
+					 <legend>修改密碼</legend>
+                      <label>請輸入密碼</label>
+                      <input name="passwd" type="password" placeholder="Password" /> 
+                      <button type="submit" class="btn">送出</button>
+				</fieldset>
+			</form>
+		</div>
+	</div>
+</div>
