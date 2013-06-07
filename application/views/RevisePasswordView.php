19a4e7da4118c87e6a08d566f01c316b216b4bf3
keming <keming@6b2d4326-5d3a-2644-a265-d5bffc151a22>
2013-06-07 15:36:35 +0000

    edit RevisePasswordView.php
    git-svn-id: https://140.124.181.190/svn/TaipeiTech@71 6b2d4326-5d3a-2644-a265-d5bffc151a22

--- application/views/RevisePasswordView.php
+++ application/views/RevisePasswordView.php
@@ -1,12 +1,29 @@
+<script>
+    $("#repassword").click(function(){
+        alert("我不能動");
+    });
+</script>
 <div class="container-fluid">
 	<div class="row-fluid">
 		<div class="span12">
 			<form>
 				<fieldset>
-					 <legend>修改密碼</legend>
-                      <label>請輸入密碼</label>
-                      <input name="passwd" type="password" placeholder="Password" /> 
-                      <button type="submit" class="btn">送出</button>
+					<legend>修改密碼</legend>
+                    <div>
+                        <label>請輸入原密碼</label>
+                        <input name="oldpasswd" type="password" placeholder="Old Password" /> 
+                    </div>
+                    <div>
+                        <label>請輸入新密碼</label>
+                        <input id="newpasswd" name="newpasswd" type="password" placeholder="New Password" /> 
+                    </div>
+                    <div>
+                        <label>再次確認</label>
+                        <input id="checkpasswd" type="password" placeholder="New Password" /> 
+                    </div>
+                    <div>
+                        <button id="repassword" type="submit" class="btn">確定修改</button>
+                    </div>
 				</fieldset>
 			</form>
 		</div>
