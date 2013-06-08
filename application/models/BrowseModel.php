2c9f5d5f5714cb9c73d63fe0c8c5da24eac581a7
gary62107 <gary62107@6b2d4326-5d3a-2644-a265-d5bffc151a22>
2013-06-08 17:52:44 +0000

    improve efficiency of BrowseModel
    git-svn-id: https://140.124.181.190/svn/TaipeiTech@94 6b2d4326-5d3a-2644-a265-d5bffc151a22

--- application/models/BrowseModel.php
+++ application/models/BrowseModel.php
@@ -10,10 +10,13 @@ class BrowseModel extends CI_Model
     
     public function GetBookByCategory($categoryID, $offset, $limit)
     {
-        $this->selectByCategory($categoryID);
+        $this->db->select('')->from('categorycorrespond')->where('cid',$categoryID);
         $data["total_num_rows"] = $this->db->count_all_results();
         
-        $this->selectByCategory($categoryID);
+        $this->db->select('c.bid,name,cover');
+        $this->db->from('categorycorrespond as c')->join('book','book.bid=c.bid','natural');
+        $this->db->where('cid',$categoryID);
+        $this->db->order_by('publishedDate','desc');
         $this->db->limit($limit,$offset);
         $data["books"] = $this->db->get();
         $data["num_rows"] = $data["books"]->num_rows();
@@ -23,29 +26,17 @@ class BrowseModel extends CI_Model
     
     public function GetLatestBook($offset, $limit)
     {
-        $this->selectLatest();
-        $data["total_num_rows"] = $this->db->count_all_results();
-        $this->selectLatest();
+        $data["total_num_rows"] = $this->db->count_all_results('book');
+        //$data["total_num_rows"] = $this->db->count_all_results();
+
+        $this->db->select('bid,name,cover');
+        $this->db->from('book');
+        $this->db->order_by('publishedDate','desc');
         $this->db->limit($limit,$offset);
         $data["books"] = $this->db->get();
         $data["num_rows"] = $data["books"]->num_rows();
         $data["books"] = $data["books"]->result();
         return $data;
-    }
-    
-    private function selectLatest()
-    {
-        $this->db->select('bid,name,cover');
-        $this->db->from('book');
-        $this->db->order_by('publishedDate','desc');
-    }
-    
-    private function selectByCategory($categoryID)
-    {
-        $this->db->select('c.bid,name,cover');
-        $this->db->from('categorycorrespond as c')->join('book','book.bid=c.bid','natural');
-        $this->db->where('cid',$categoryID);
-        $this->db->order_by('publishedDate','desc');
     }
 }
 ?>
