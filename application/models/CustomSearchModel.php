e7b2388508083dd55b5e563ab5663a0d4b429c94
ben <ben@6b2d4326-5d3a-2644-a265-d5bffc151a22>
2013-06-10 05:09:57 +0000

    git-svn-id: https://140.124.181.190/svn/TaipeiTech@126 6b2d4326-5d3a-2644-a265-d5bffc151a22

--- application/models/CustomSearchModel.php
+++ application/models/CustomSearchModel.php
@@ -11,54 +11,60 @@ class CustomSearchModel extends CI_Model
     
     public function searchByAuthor($authorName)
     {
-        $this->db->select('B.bid, B.name, B.cover, B.publishedDate, B.price, B.ISBN, B.onShelf');
-        $this->db->from('BOOK AS B, AUTHOR AS A, WRITERCORRESPOND AS W ');
-        $this->db->where("A.name = '$authorName' AND W.aid = A.aid AND B.bid = W.bid");
+        $this->db->select('B.bid, B.name, A.name as author, B.cover, p.name as publisher, B.publishedDate, B.price, B.ISBN, B.onShelf');
+        $this->db->from('BOOK AS B, AUTHOR AS A, WRITERCORRESPOND AS W, publisher as p');
+        $this->db->like('A.name', "$authorName");
+        $this->db->where("W.aid = A.aid AND B.bid = W.bid AND B.pid = P.pid");
+        
         $data = $this->db->get();
         return $data;
     }
     
     public function searchByName($name)
     {
-        $this->db->select('B.bid, B.name, B.cover, B.publishedDate, B.price, B.ISBN, B.onShelf');
-        $this->db->from('BOOK AS B');
-        $this->db->where("B.name = '$name'");
+        $this->db->select('B.bid, B.name, a.name as author, B.cover, p.name as publisher, B.publishedDate, B.price, B.ISBN, B.onShelf');
+        $this->db->from('BOOK AS B, author as a, WRITERCORRESPOND AS W, publisher as p ');
+        $this->db->like('B.name', "$name");
+        $this->db->where("W.aid = A.aid AND B.bid = W.bid AND B.pid = P.pid");
+        //$this->db->where("B.name = '$name'");
         $data = $this->db->get();
         return $data;
     }
     
     public function searchByBooksellers($sellerName)
     {
-        $this->db->select('B.bid, B.name, B.cover, B.publishedDate, B.price, B.ISBN, B.onShelf');
-        $this->db->from('BOOK AS B, PUBLISHER AS P');
-        $this->db->where("P.name = '$sellerName' AND B.pid = P.pid");
+        $this->db->select('B.bid, B.name, a.name as author, B.cover, p.name as publisher, B.publishedDate, B.price, B.ISBN, B.onShelf');
+        $this->db->from('BOOK AS B, PUBLISHER AS P, author as a, WRITERCORRESPOND AS W');
+        $this->db->like('P.name', "$sellerName");
+        $this->db->where("B.pid = P.pid AND W.aid = A.aid AND B.bid = W.bid");
         $data = $this->db->get();
         return $data;
     }
     
     public function searchByPublishedDate($publishedDate)
     {
-        $this->db->select('B.bid, B.name, B.cover, B.publishedDate, B.price, B.ISBN, B.onShelf');
-        $this->db->from('BOOK AS B');
-        $this->db->where("B.publishedDate = '$publishedDate'");
+        $this->db->select('B.bid, B.name, a.name as author, B.cover, p.name as publisher, B.publishedDate, B.price, B.ISBN, B.onShelf');
+        $this->db->from('BOOK AS B, author as a, WRITERCORRESPOND AS W, publisher as p');
+        $this->db->where("B.publishedDate = '$publishedDate' AND W.aid = A.aid AND B.bid = W.bid AND B.pid = P.pid");
         $data = $this->db->get();
         return $data;
     }
     
     public function searchByCategory($category)
     {
-        $this->db->select('B.bid, B.name, B.cover, B.publishedDate, B.price, B.ISBN, B.onShelf');
-        $this->db->from('BOOK AS B, CATEGORY AS C, CATEGORYCORRESPOND AS CC');
-        $this->db->where("C.name = '$category' AND CC.cid = C.cid AND B.bid = CC.bid");
+        $this->db->select('B.bid, B.name, a.name as author, B.cover, p.name as publisher, B.publishedDate, B.price, B.ISBN, B.onShelf');
+        $this->db->from('BOOK AS B, CATEGORY AS C, CATEGORYCORRESPOND AS CC, author as a, WRITERCORRESPOND AS W, publisher as p');
+        $this->db->like('C.name', $category);
+        $this->db->where("CC.cid = C.cid AND B.bid = CC.bid AND W.aid = A.aid AND B.bid = W.bid AND B.pid = P.pid");
         $data = $this->db->get();
         return $data;
     }
     
     public function searchByISBN($isbn)
     {
-        $this->db->select('B.bid, B.name, B.cover, B.publishedDate, B.price, B.ISBN, B.onShelf');
-        $this->db->from('BOOK AS B');
-        $this->db->where("B.ISBN = '$isbn'");
+        $this->db->select('B.bid, B.name, a.name as author, B.cover, p.name as publisher, B.publishedDate, B.price, B.ISBN, B.onShelf');
+        $this->db->from('BOOK AS B, author as a, WRITERCORRESPOND AS W, publisher as p');
+        $this->db->where("B.ISBN = '$isbn' AND W.aid = A.aid AND B.bid = W.bid AND B.pid = P.pid");
         $data = $this->db->get();
         return $data;
     }
--- application/views/CustomSearch/browse.php
+++ application/views/CustomSearch/browse.php
@@ -3,8 +3,10 @@
     foreach ($records->result() as $row)
     {
         echo "bid:" . $row->bid;
-        echo "name: " . $row->name;
+        echo ", name: " . iconv("UTF-8","big5",$row->name);
+        echo ", author: " . iconv("UTF-8","big5",$row->author);
         echo '<img src="data:image/jpeg;base64,'.base64_encode($row->cover).'" alt="photo">';
+        echo ", publisher: " . iconv("UTF-8","big5",$row->publisher);
         echo ", publishedDate: " . $row->publishedDate;
         echo ", price: " . $row->price;
         echo ", ISBN: " . $row->ISBN;
