e0bdf0bc8d5be2239b6fbdccb44dfc52f4dfc406
kai <kai@6b2d4326-5d3a-2644-a265-d5bffc151a22>
2013-06-11 12:36:11 +0000

    administrator can modify account authority
    git-svn-id: https://140.124.181.190/svn/TaipeiTech@160 6b2d4326-5d3a-2644-a265-d5bffc151a22

--- application/views/AccountManagementView.php
+++ application/views/AccountManagementView.php
@@ -17,6 +17,16 @@ function setAccountAvaliable(mid, avaliable){
                 });       
         }
 }
+
+function setAccountAuthority(mid, authority){
+    console.log(mid + "," + authority);
+    $.ajax({url: "<?= base_url("Account/modifyAuthority") ?>/"+mid +"/" + authority}).
+            done(function(data){
+                if(data != "OK")
+                    //alert(data);
+                    location.reload();
+            });  
+}
 </script>
 
 <div class="container-fluid">
@@ -60,7 +70,33 @@ function setAccountAvaliable(mid, avaliable){
 							<?= $account->email ?>
 						</td>
 						<td>
-							<?= $account->authority ?>
+                            <?php
+                            if($account->authority=="administrator"){
+                            ?>
+                                <select onchange="setAccountAuthority(<?=$account->mid?>,this.options[this.selectedIndex].value)">
+                                    <option value="customer" onclick="setAccountAuthority()">Customer</option>
+                                    <option value="manager">Manager</option>
+                                    <option value="administrator" selected="true">Administrator</option>
+                                </select>
+                            <?php
+                            }else if($account->authority=="manager"){
+                            ?>
+                                <select onchange="setAccountAuthority(<?=$account->mid?>,this.options[this.selectedIndex].value)">
+                                    <option value="customer">Customer</option>
+                                    <option value="manager" selected="true">Manager</option>
+                                    <option value="administrator">Administrator</option>
+                                </select>
+                            <?php
+                            }else{
+                            ?>
+                                <select onchange="setAccountAuthority(<?=$account->mid?>,this.options[this.selectedIndex].value)">
+                                    <option value="customer" selected="true">Customer</option>
+                                    <option value="manager">Manager</option>
+                                    <option value="administrator">Administrator</option>
+                                </select>
+                            <?php
+                            }
+                            ?>
 						</td>
                         <?php
                         if ($account->available == 0) {
