2bff6ba9a49030a652d8166701d141c0410fefd4
kai <kai@6b2d4326-5d3a-2644-a265-d5bffc151a22>
2013-06-12 13:08:48 +0000

    fix unicode and ansi bug
    git-svn-id: https://140.124.181.190/svn/TaipeiTech@203 6b2d4326-5d3a-2644-a265-d5bffc151a22

--- application/controllers/TransactionRecords.php
+++ application/controllers/TransactionRecords.php
@@ -7,16 +7,35 @@ class TransactionRecords extends CI_Controller
         $this->load->model("template");
         $this->load->model("TransactionModel");
         $this->load->model("MenuModel");
+        $this->load->library("pagination");
     }
 
     public function index($offset = 0)
     {
         $slideBarList = $this->MenuModel->getManagerList();
         $slideBarList["TransactionRecords"]['Active'] = "active";
+        $this->initPaginationConfig($config);
+        $config['total_rows'] = $this->TransactionModel->getTransactionRecordAmount();
+        $this->pagination->initialize($config);
 
         $content = "TransactionRecordsView";
         $data["list"] = $this->TransactionModel->browseTransactionRecords();
+        $data['pagination'] = $this->pagination->create_links();
         $this->template->loadView("Manager", $slideBarList, $content, $data);
+    }
+
+    private function initPaginationConfig(&$config)
+    {
+        $config['per_page'] = 20;
+        $config['num_links'] = 5;
+        $config['num_tag_open'] = $config['prev_tag_open'] = $config['next_tag_open'] =
+            $config['first_tag_open'] = $config['last_tag_open'] = '<li>';
+        $config['num_tag_close'] = $config['prev_tag_close'] = $config['next_tag_close'] =
+            $config['first_tag_close'] = $config['last_tag_close'] = '</li>';
+        $config['cur_tag_open'] = "<li = class='active'><a href='#'>";
+        $config['cur_tag_close'] = "</a>";
+        $config['full_tag_open'] = '<div class="pagination pagination-centered"><ul>';
+        $config['full_tag_close'] = '</ul></div>';
     }
 }
 
